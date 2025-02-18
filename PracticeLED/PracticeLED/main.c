51ce87c316eb84cc0981f80b11680a7bf6558f8a
avifreakinash <avinash.pb.mail@gmail.com>
2025-02-18 22:36:34 +0530

    added Functions to process button Press and Release and moved the code out of main to enhance readability

--- PracticeLED/PracticeLED/main.c
+++ PracticeLED/PracticeLED/main.c
@@ -24,12 +24,18 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+void ProcessButtonPress();
+void ProcessButtonRelease();
+
 uint8_t LED ;
 uint8_t count;						 
 uint16_t Stream_of_Zeros;		// When button is Pressed, the micro controller button pin goes LOW and generates a streams of few "Zero and Ones" before finally coming to Zero, and
 								// creating a stable stream of Zeros until the button is not Released
 uint16_t Streams_of_Ones;		// When Button is Released, the micro controller button pin goes HIGH and generates a streams of few "Ones and Zeros" before finally coming to ONE, and
 								// creating a stable stream of ONnes until the button is Released
+								
+						
+
 int main(void)
 {
 		DDRD |= 0b01111111;		// Setting PINS as Output for LEDs
@@ -40,41 +46,48 @@ int main(void)
 	
 	while (1)
 	{				
-		if (  bit_is_clear(PINB , 0) )				//  If Button is Pressed  
-		{
-			Stream_of_Zeros++;
-			if (Stream_of_Zeros >= 500)
-			{	
-				if(count == 0)		//	Temp variable to make sure loop executes only once - if this is not present, all LEDs will lit up with just one press,
-									//  as streams of zero's still overflows 500 count. The count is Reset in else statement to initialize initial state of count =0  
-				{
-					count = 1;		//  Count is Set to execute this loop exactly once, to get button toggle effect
-					PORTD |= 1<<LED;
-					LED++;
-					if (LED > 6)
-					{
-						for( uint8_t i=0;i<5;i++)
-						{
-							PORTD = 0b11111111;
-							_delay_ms(100);
-							PORTD = 0b00000000;
-							_delay_ms(100);
-						}
-						LED = 0; 
-					}
-				}
-				Stream_of_Zeros = 0;
-			}
-		}
-		else											// if Button is Released 
-		{
-			Streams_of_Ones++;			 // Count a good amount of continuous ONEs to be sure that button is actually released
-			if (Streams_of_Ones >= 500)
-			{
-				count = 0;		// Reset counter
-				Streams_of_Ones = 0;
-			}
-		}
+		if (  bit_is_clear(PINB , 0) )					//  If Button is Pressed  
+					ProcessButtonPress();
+		else											//  if Button is Released 
+					ProcessButtonRelease();
 	}
 }
-		
+	
+									
+void ProcessButtonPress()
+{
+	Stream_of_Zeros++;
+	if (Stream_of_Zeros >= 500)
+	{
+		if(count == 0)		//	Temp variable to make sure loop executes only once - if this is not present, all LEDs will lit up with just one press,
+							//  as streams of zero's still overflows 500 count. The count is Reset in else statement to initialize initial state of count =0
+		{
+			count = 1;		//  Count is Set to execute this loop exactly once, to get button toggle effect
+			PORTD |= 1<<LED;
+			LED++;
+			if (LED > 6)
+			{
+				for( uint8_t i=0;i<5;i++)
+				{
+					PORTD = 0b11111111;
+					_delay_ms(100);
+					PORTD = 0b00000000;
+					_delay_ms(100);
+				}
+				LED = 0;
+			}
+		}
+		Stream_of_Zeros = 0;
+	}
+}								
+
+void ProcessButtonRelease()
+{
+	Streams_of_Ones++;			 // Count a good amount of continuous ONEs to be sure that button is actually released
+	if (Streams_of_Ones >= 500)
+	{
+		count = 0;		// Reset counter
+		Streams_of_Ones = 0;
+	}
+}
+	
