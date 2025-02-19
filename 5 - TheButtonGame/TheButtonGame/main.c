f0fe88122b60afc292f64d4a958725a920d84d7e
avifreakinash <avinash.pb.mail@gmail.com>
2025-02-19 13:18:34 +0530

    Refactored Code

--- 5 - TheButtonGame/TheButtonGame/main.c
+++ 5 - TheButtonGame/TheButtonGame/main.c
@@ -19,39 +19,88 @@
 #define F_CPU 1000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-void ProcessButtonPressed(int Button_id);
-void ProcessButtonReleased(int Button_id);
-void Blink_All_LEDs(volatile uint8_t *port);
 
+void ProcessButtonPress(volatile uint8_t button_id);
+void ProcessButtonRelease(volatile uint8_t button_id);
+void Blink_port_LEDs (volatile uint8_t *port);
+void Reset_all_LED_Counter ();
 
-int LED_Number[2] ;
-int Pressed[2];			// Counter to hold the state of LEDs until button is pressed again
-int Stream_of_Zeros[2];			// When button is Pressed, the micro controller button pin goes LOW and generates a streams of few "Zero and Ones" before finally coming to Zero, and
-								// creating a stable stream of Zeros until the button is not Released
-int Streams_of_Ones[2];			// When Button is Released, the micro controller button pin goes HIGH and generates a streams of few "Ones and Zeros" before finally coming to ONE, and
-								// creating a stable stream of ONnes until the button is Released
+uint8_t LED[2] ;
+volatile uint8_t count[2];
+uint16_t Stream_of_Zeros[2];		// When button is Pressed, the micro controller button pin goes LOW and generates a streams of few "Zero and Ones" before finally coming to Zero, and
+									// creating a stable stream of Zeros until the button is not Released
+uint16_t Streams_of_Ones[2];		// When Button is Released, the micro controller button pin goes HIGH and generates a streams of few "Ones and Zeros" before finally coming to ONE, and
+									// creating a stable stream of ONnes until the button is Released
+
 int main(void)
 {
-		DDRD |= 0b01111111;		// Setting PINS as Output for LEDs
-		DDRC |= 0b01111111;
-
-		PORTD	=	 0x00;		// Setting LEDs OFF initially
-		PORTC	=	 0x00;
-
-		DDRB	&=	 ~(1<< PINB0) |  ~(1<< PINB1);	// Initialize BUTTON pin
-		PORTB	|=	 (1 << PINB0) | (1 << PINB1) ;	// Ensure pin is HIGH (PULLED UP) initially
+	DDRD |= 0b01111111;		// Setting PINS as Output for LEDs
+	DDRC |= 0b01111111;
+	PORTD	=	 0x00;		// Setting LEDs OFF initially
+	PORTC	=	 0x00;
+	
+	DDRB	&=	 ~( (1<< PINB0) | (1<< PINB1) );	// Initialize BUTTON pin
+	PORTB	|=	 (1 << PINB0) | (1 << PINB1) ;	// Ensure pin is HIGH (PULLED UP) initially
 	
 	while (1)
-	{				
-		if (  bit_is_clear(PINB , 0) )		ProcessButtonPressed(0);		 //  If Button is Pressed on SIDE 1  
-		else	ProcessButtonReleased(0);										// if Button is Released 
-		 
-		if (  bit_is_clear(PINB , 1) )		ProcessButtonPressed(1);		 //  If Button is Pressed on SIDE 2
-		else	ProcessButtonReleased(1);										// if Button is Released	
+	{
+		if (  bit_is_clear(PINB , 0) )	ProcessButtonPress(0);		//  If Button is Pressed
+		else	ProcessButtonRelease(0);							//  if Button is Released
+		
+		if (  bit_is_clear(PINB , 1) )	ProcessButtonPress(1);
+		else	ProcessButtonRelease(1);
 	}
 }
 
-void Blink_All_LEDs (volatile uint8_t *port)
+
+void ProcessButtonPress(uint8_t button_id)
+{
+	Stream_of_Zeros[button_id]++;
+	if (Stream_of_Zeros[button_id] >= 500)
+	{
+		if(count[button_id] == 0)		//	Temp variable to make sure loop executes only once - if this is not present, all LEDs will lit up with just one press,
+										//  as streams of zero's still overflows 500 count. The count is Reset in else statement to initialize initial state of count =0
+		{
+			count[button_id] = 1;		//  Count is Set to execute this loop exactly once, to get button toggle effect
+			
+			if(button_id == 0)
+			{
+				PORTD |= 1<<LED[0];		//	Shift Leds of PORT D
+				LED[0]++;
+				if (LED[0] > 6)
+				{
+					Blink_port_LEDs (&PORTD);
+					Reset_all_LED_Counter ();
+					PORTC = 0b00000000;
+				}
+			}
+			if(button_id == 1)
+			{
+				PORTC |= 1<<LED[1];		//	Shift Leds of PORT C
+				LED[1]++;
+				if (LED[1] > 6)
+				{
+					Blink_port_LEDs (&PORTC);
+					Reset_all_LED_Counter ();
+					PORTD = 0b00000000;
+				}
+			}
+		}
+		Stream_of_Zeros[button_id] = 0;
+	}
+}
+
+void ProcessButtonRelease(uint8_t button_id)
+{
+	Streams_of_Ones[button_id]++;			 // Count a good amount of continuous ONEs to be sure that button is actually released
+	if (Streams_of_Ones[button_id] >= 500)
+	{
+		count[button_id] = 0;		// Reset counter
+		Streams_of_Ones[button_id] = 0;
+	}
+}
+
+void Blink_port_LEDs (volatile uint8_t *port)
 {
 	for( uint8_t i=0;i<5;i++)
 	{
@@ -62,49 +111,8 @@ void Blink_All_LEDs (volatile uint8_t *port)
 	}
 }
 
-
-void ProcessButtonPressed(int Button_id)
+void Reset_all_LED_Counter ()
 {
-	Stream_of_Zeros[Button_id]++;					// Count a good amount of continuous ZEROs to be sure that button is actually pressed
-	if (Stream_of_Zeros[Button_id] >= 500)
-	{
-		if (Pressed[Button_id] == 0)		// when counter = 0 from previous else call or initially	Pressed[0] == 0
-												//															Pressed[1] == 0
-		{
-			Pressed[Button_id] = 1;			// set counter
-			if (Button_id == 0)
-			{
-				PORTD |=	 1<<LED_Number[Button_id];	// LED_Number[0] 
-				LED_Number[Button_id]++;
-				if (LED_Number[Button_id] > 6)
-				{
-					Blink_All_LEDs( &PORTD );
-					LED_Number[Button_id] = 0;
-				}
-			}
-			if (Button_id == 1)
-			{
-				PORTC |=	 1<<LED_Number[Button_id];
-				LED_Number[Button_id]++;
-				if (LED_Number[Button_id] > 6)
-				{
-					Blink_All_LEDs( &PORTC );
-					LED_Number[Button_id] = 0;
-				}
-			}
-			
-			
-		}
-		Stream_of_Zeros[Button_id] =0;
-	}
+	LED[1] = 0;
+	LED[0] = 0;
 }
-
- void ProcessButtonReleased(int Button_id)
- {
-	 Streams_of_Ones[Button_id]++;			 // Count a good amount of continuous ONEs to be sure that button is actually released
-	 if (Streams_of_Ones[Button_id] >= 500)
-	 {
-		 Pressed[Button_id] = 0;		// Reset counter
-		 Streams_of_Ones[Button_id] = 0;
-	 }
- }
